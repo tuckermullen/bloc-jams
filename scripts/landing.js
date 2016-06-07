8e08da6fe49c357b52f45f41d1ceb9c0a4df6168
Tucker Mullen <mullentucker@gmail.com>
2016-06-07 16:55:41 -0700

    Assignment 22 complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -7,6 +7,7 @@ var animatePoints = function() {
         points[index].style.msTransform = "scaleX(1) translateY(0)";
         points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
     }
+    
 
     for (var i = 0; i < points.length; i++) {
         revealPoint(i);
