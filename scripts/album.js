32aa5c3de40a4e5486d12deba3e57afb7a5d58d5
tuckermullen <mullentucker@gmail.com>
2016-06-28 19:56:52 -0700

    Assignment 34 complete

--- scripts/album.js
+++ scripts/album.js
@@ -228,7 +228,7 @@ var updatePlayerBarSong = function() {
     $('.currently-playing .artist-name').text(currentAlbum.artist);
     $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.title + " - " + currentAlbum.artist);
     $('.main-controls .play-pause').html(playerBarPauseButton);
-    setTotalTimeInPlayerBar(filterTimeCode(currentSongFromAlbum.length));
+    setTotalTimeInPlayerBar(filterTimeCode(currentSongFromAlbum.duration));
 };
 
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
